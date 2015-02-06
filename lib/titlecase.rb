49cd377bcbee367726ee15f4e89df7966863e8b0
David Zulaica <zulaica@gmail.com>
2015-02-06 01:50:34 -0800

    Refactor and cleanup.

--- lib/titlecase.rb
+++ lib/titlecase.rb
@@ -14,16 +14,16 @@ class String
                   "with"
                   ]
 
-  def new_titlecase
-    the_string = self.split
-    the_string.at(0).capitalize!
-    the_string.last.capitalize!
-    the_string.each do |word|
+  def titlecase
+    string = self.split
+    string.first.capitalize!
+    string.last.capitalize!
+    string.each do |word|
       unless @@small_words.include?(word)
         word.capitalize!
       end
     end
-    the_string.join(" ")
+    string.join(" ")
   end
 
 end
---
+++ spec/titlecase_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe String do
+
+  describe "#titlecase" do
+    it "will properly titlecase a string" do
+      string = "dog walks into a wall"
+      expect(string.titlecase).to eq "Dog Walks Into a Wall"
+    end
+
+    it "will always capitalize the first word of a title" do
+      string = "the quick brown fox jumps over the lazy dog"
+      expect(string.titlecase).to eq "The Quick Brown Fox Jumps Over the Lazy Dog"
+    end
+
+    it "will always capitalize the last word of a title" do
+      string = "there is nothing to be afraid of"
+      expect(string.titlecase).to eq "There Is Nothing to Be Afraid Of"
+    end
+  end
+
+end
